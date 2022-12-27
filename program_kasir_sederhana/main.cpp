65d961b0cfbdabb2f4a0f013747b54aa6c8081ce
sleepynoid <aliffarhan003@gmail.com>
2022-12-28 06:03:49 +0700

    adds transaction (incomplete)

--- program_kasir_sederhana/main.cpp
+++ program_kasir_sederhana/main.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <stdio.h>
 #include <cstring>
+#include <string>
 using namespace std;
+// variabel global
 char nama_barang[10][20] = {
-    "Ikan",
+    "ikan",
     "ayam",
     "bebek"
 };
 int harga_barang[10] = {
-    5000,
-    10000,
-    18000
+    16000,
+    18000,
+    15000
 };
 int stock_barang[10] = {
     10,
@@ -18,6 +20,7 @@ int stock_barang[10] = {
     10
 };
 int Index = 2;
+// fungsi
 void view_barang() {
     printf("List Barang\n");
     for (int i=0; i <= Index; i++) {
@@ -49,12 +52,45 @@ void delete_barang() {
     }
     (_find_delete <= Index)? Index--:printf("Maaf Barang %s Tidak ada\n",_delete);
 }
+int search_by_name(string name){
+    int temp_index = -1;
+    for (int i = 0 ; i <= Index ; i++){
+        if (name == nama_barang[i]){
+            temp_index = i;
+        }
+    }
+    return temp_index;
+}
+void update_barang() {
+    printf("Masukan Nama Barang yang akan di Update : ");
+    char nama[20];
+    scanf("%s",nama);
+    int n,temp_index = search_by_name(nama);
+    printf("Masukan Stock %s = ",nama);
+    scanf("%d",&n);
+    (temp_index != -1)? stock_barang[temp_index] = n:printf("Maaf Barang %s tidak tersedia\n",nama);
+    // if (temp_index != -1) {
+    //     printf("Masukan Stock %s = ",nama);
+    //     scanf("%d",&n);
+    //     stock_barang[temp_index] = n;
+    // }
+    // else {
+    //     printf("Maaf Barang %s tidak tersedia\n",nama);
+    // }
+}
+int transaksi(string name,int banyak_benda) {
+    printf("nama barang %s, banyak %d",name.c_str(),banyak_benda);
+    
+    return banyak_benda;
+}
+
 int main() {
     printf("MENU PROGRAM\n");
     printf("1. view all BARANG\n");
     printf("2. tambah BARANG\n");
     printf("3. delete BARANG\n");
     printf("4. update stok BARANG\n");
+    printf("5. transaksi\n");
     printf("5. EXIT\n");
     int pilihan;
     while (pilihan != 5) {
@@ -69,6 +105,20 @@ int main() {
             break;
         case 3:
             delete_barang();
+            break;
+        case 4:
+            update_barang();
+            break;
+        case 5:
+            printf("Masukan Nama Barang : ");
+            string name;
+            cin >> name;
+            int n = search_by_name(name);
+            printf("Masukan Jumlah Barang : ");
+            scanf("%d",&n);
+            if (n != -1) {
+                transaksi(name, n);
+            }
             break;
         }
     }
